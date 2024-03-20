7912845170964e66c676707f1f89c70b4187ef44
sreenath <sreenathbr20@gmail.com>
2024-03-20 11:29:56 +0530

    first commit

---
+++ eureka-client2/src/main/java/com/eurekaclient1/Service1Controller.java
@@ -0,0 +1,13 @@
+package com.eurekaclient1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Service1Controller {
+
+	@GetMapping("/")
+	public String greetings() {
+		return "welcome to service 2";
+	}
+}
